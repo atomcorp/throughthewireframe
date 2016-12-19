22f2ece670b6871fe13722e5a759aaf1872946a9
atomcorp <thomasmaxwellsmith@gmail.com>
2016-12-19 23:09:30 +0000

    added compiled css and js for other people to quickly use, mostly did a facebook page as an example

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,6 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish'); // add color to jshint output
 var include = require('gulp-include');
 // css + js
-// var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var notify = require('gulp-notify');
 
@@ -24,7 +23,9 @@ var paths = {
     jsSrc: 'source/js/*.js',
     jsComponents: 'source/js/*/*.js',
     jsPlugins: 'source/plugins/*.js',
-    dist: 'dist/'
+    dist: 'dist/',
+    distCompile: 'compile/',
+    sassCompile: 'source/sass/compile/*.scss',
 };
 
 // Production style
@@ -64,6 +65,32 @@ gulp.task('sass', function () {
     }
 });
 
+gulp.task('sassCompile', function () {
+    if (!inProduction) {
+        gulp.src(paths.sassCompile)
+            .pipe(plumber({errorHandler: onError}))
+            .pipe(sourcemaps.init())
+                .pipe(sass())
+                .pipe(autoprefixer({
+                    browsers: ['> 1%', 'IE 7'], 
+                    remove: true 
+                }))
+            .pipe(sourcemaps.write())
+            // .pipe(gzip())
+            .pipe(gulp.dest(paths.distCompile));
+    } else {
+        gulp.src(paths.sassCompile)
+            .pipe(plumber({errorHandler: onError}))
+            .pipe(sass({outputStyle: 'compressed'}))
+            .pipe(autoprefixer({
+                browsers: ['> 1%', 'IE 7'], 
+                remove: true 
+            }))
+        // .pipe(gzip())
+        .pipe(gulp.dest(paths.distCompile));
+    }
+});
+
 gulp.task('js',function() {
     if (!inProduction) { // not in production
         return gulp.src([paths.jsPlugins, paths.jsSrc])
@@ -73,7 +100,8 @@ gulp.task('js',function() {
             .pipe(include())
             .pipe(concat('js.js'))
             // .pipe(gzip())
-            .pipe(gulp.dest(paths.dist));
+            .pipe(gulp.dest(paths.dist))
+            .pipe(gulp.dest(paths.distCompile));
     } else {
         return gulp.src([paths.jsPlugins, paths.jsSrc])
             .pipe(plumber({errorHandler: onError}))
@@ -81,7 +109,28 @@ gulp.task('js',function() {
             .pipe(uglify())
             .pipe(concat('js.js'))
             // .pipe(gzip())
-            .pipe(gulp.dest(paths.dist));
+            .pipe(gulp.dest(paths.dist))
+    }
+});
+
+gulp.task('jsCompile',function() {
+    if (!inProduction) { // not in production
+        return gulp.src([paths.jsPlugins, paths.jsSrc])
+            .pipe(plumber({errorHandler: onError}))
+            // .pipe(jshint())
+            // .pipe(jshint.reporter(stylish))
+            .pipe(include())
+            .pipe(concat('wire.js'))
+            // .pipe(gzip())
+            .pipe(gulp.dest(paths.distCompile));
+    } else {
+        return gulp.src([paths.jsPlugins, paths.jsSrc])
+            .pipe(plumber({errorHandler: onError}))
+            .pipe(include())
+            .pipe(uglify())
+            .pipe(concat('wire.js'))
+            // .pipe(gzip())
+            .pipe(gulp.dest(paths.distCompile));
     }
 });
 
@@ -95,3 +144,5 @@ gulp.task('watch',function() {
 // run sass + js watch for any more changes
 // gulp.task('default', ['sass', 'js', 'watch']);
 gulp.task('default', ['sass', 'watch']);
+
+gulp.task('compile', ['sassCompile','jsCompile']);
